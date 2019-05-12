c31adafdc8f65670beefb2cf468e88d9c21d3d54
Bruno Alla <alla.brunoo@gmail.com>
2019-05-12 22:02:18 +0100

    Enable warnings when running tests

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -23,7 +23,7 @@ docker-compose -f local.yml run django mypy my_awesome_project
 docker-compose -f local.yml run django black --check --diff  --exclude 'migrations' ./
 
 # run the project's tests
-docker-compose -f local.yml run django pytest
+docker-compose -f local.yml run django pytest -Wd
 
 # return non-zero status code if there are migrations that have not been created
 docker-compose -f local.yml run django python manage.py makemigrations --dry-run --check || { echo "ERROR: there were changes in the models, but migration listed above have not been created and are not saved in version control"; exit 1; }
